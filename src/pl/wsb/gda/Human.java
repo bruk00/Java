aa8ca0dce0b065ba52a37e928dc9308ebb1df66e
bruk00 <kacperkaszubowski@gmail.com>
2021-02-28 10:41:59 +0100

    a

---
+++ src/pl/wsb/gda/Human.java
@@ -0,0 +1,13 @@
+package pl.wsb.gda;
+
+public class Human {
+    String firstName;
+    String lastName;
+    String position;
+
+    public Human(String firstName, String lastName, String position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+    }
+}
